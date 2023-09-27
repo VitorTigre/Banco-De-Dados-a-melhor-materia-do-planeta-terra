ee8343dcdb33c31b7fc4f1f543a054a93ec709b7
VitorTigre <102634931+VitorTigre@users.noreply.github.com>
2023-09-26 23:24:54 -0300

    2. Livros por Categoria

--- listaExercícios02.sql
+++ listaExercícios02.sql
@@ -6,3 +6,15 @@
    DELIMITER ;
 
    CALL sp_ListarAutores();
+
+DELIMITER //
+   CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+   BEGIN
+       SELECT Livro.Titulo
+       FROM Livro
+       INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+       WHERE Categoria.Nome = categoriaNome;
+   END //
+   DELIMITER ;
+
+   CALL sp_LivrosPorCategoria('Romance');
